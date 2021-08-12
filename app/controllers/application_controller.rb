ee0fbf5be0af0554b09c6be40a5d5954445592f2
miwakos <goma.kabo@gmail.com>
2021-08-12 09:50:00 +0900

    BASIC認証の導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
 
   def after_sign_up_path_for(resource)
     new_user_information_path
+  end
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 
 end
