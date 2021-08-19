083706b153ec70defdd6bdcc43216142befc4201
miwakos <goma.kabo@gmail.com>
2021-08-20 07:35:21 +0900

    フィルタ用jsファイル作成

--- app/javascript/book_tags.js
+++ app/javascript/book_tags.js
@@ -1,25 +1,16 @@
 window.addEventListener('DOMContentLoaded', () => {
-  let tags = document.getElementsByClassName("category-name");
-  tagsAry = Array.prototype.slice.call(tags);
-
-  function tagSwitch() {
-    // 全てのactiveクラスのうち、最初の要素を削除
-    document.getElementsByName("active")[0].classList.remove("active");
-    // クリックしたタブにactiveクラスを追加
-    this.classList.add("active");
-
-    // 全てのshowクラスのうち、最初の要素を削除
-    document.getElementsByClassName("show")[0].classList.remove("show");
-    // 何番目の要素がクリックされたかを、配列tabsから要素番号を取得
-    const index = tagsAry.indexOf(this);
-
-    // クリックしたbook-contentsクラスにshowクラスを追加する
-    document.getElementsByClassName("book-contents")[index].classList.add("show");
-  };
-
-  // タグの中でクリックイベントが発生した場所を探し、tagSwitch関数を呼び出す
-  tagsAry.forEach(function(value) {
-    value.addEventListener("click", tagSwitch);
+  // .tagがクリックされたときを指定
+  $('.category-name').click(function(){
+    // 今ある.activeを削除
+    console.log('クリック')
+    $('.active').removeClass('active');
+    // クリックされた.tagに.activeを追加
+    $(this).addClass('active');
+    // 今ある.showを削除
+    $('.show').removeClass('show');
+    // indexに.tagのindex番号を代入
+    const index = $(this).index();
+    // .book-contents とindexの番号が同じ要素に.showを追加
+    $('.book-contents ').eq(index).addClass('show');
   });
-
 });
