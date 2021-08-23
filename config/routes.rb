001e04f8d232e348491216a42ae51d2770590630
miwakos <goma.kabo@gmail.com>
2021-08-23 23:59:15 +0900

    コントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: 'homes#index'
 
   resources :homes, only: :index
+  resources :user, only: :show
   resources :user_informations, only: [:new, :create]
   resources :lounges, only: [:index, :show, :destroy] do
     resources :chat_messages, only: [:create]
